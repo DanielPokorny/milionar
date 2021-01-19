bb6a42006efe14c956e22de16274a50b8f72d289
Daniel Pokorný <mr.pokorny.d@gmail.com>
2021-01-19 13:04:37 +0100

    Vytvoren Pair

--- src/com/pokesoft/DecisionTree.java
+++ src/com/pokesoft/DecisionTree.java
@@ -1,4 +1,32 @@
 package com.pokesoft;
 
 public class DecisionTree {
+    private class Pair {
+        private float value;
+        private String category;
+
+        public Pair(float value, String category) {
+            this.value = value;
+            this.category = category;
+        }
+
+        public float getValue() {
+            return value;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setValue(float value) {
+            this.value = value;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+    }
+    public void addData(float value, String category){
+
+    }
 }
