ae3b3c92b89713d96a2ef607522b1b50ae194870
Daniel Pokorný <mr.pokorny.d@gmail.com>
2021-07-03 17:56:31 +0200

    promazane todo

--- src/main/java/com/pokesoft/Main.java
+++ src/main/java/com/pokesoft/Main.java
@@ -16,9 +16,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
-//todo nacitani nahodneho poctu zapasu
 //todo tipy i pro a 10 a 02
-//todo zapasy jen na zitra
 
 public class Main {
 
